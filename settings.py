615412b3bfd9ec52b95c2851ded16415d6d63799
Finn Phillips <finnphillips656@gmail.com>
2024-01-30 10:02:58 +0000

    The basic movement systems, as well as the player rotation, and hit boxes.

---
+++ settings.py
@@ -0,0 +1,14 @@
+# game setup
+WIDTH = 1280
+HEIGHT = 720
+FPS = 60
+
+# player setup
+PLAYER_START_X = 400
+PLAYER_START_Y = 500
+
+# player size
+PLAYER_SIZE = 0.25
+
+# player speed
+PLAYER_SPEED = 6
