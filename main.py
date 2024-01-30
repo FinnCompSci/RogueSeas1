327eb586d9dfbce17a41f365dbd9d4b670a55bff
Finn Phillips <finnphillips656@gmail.com>
2024-01-30 10:08:09 +0000

    The basic movement systems, as well as the player rotation, and hit boxes.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Player(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(PLAYER_START_X, PLAYER_START_Y)
         self.image = pygame.transform.rotozoom(pygame.image.load("Pirateship2.png").convert_alpha(), 0, PLAYER_SIZE)
         self.base_player_image = self.image
-        self.hitbox_rect = self.base_player_image.get_rect(center = self.pos)
+        self.hitbox_rect = self.base_player_image.get_rect(center=self.pos)
         self.rect = self.hitbox_rect.copy()
         self.speed = PLAYER_SPEED
 
@@ -27,7 +27,7 @@ class Player(pygame.sprite.Sprite):
         self.y_change_mouse_player = (self.mouse_coords[1] - self.hitbox_rect.centery)
         self.angle = math.degrees(math.atan2(self.y_change_mouse_player, self.x_change_mouse_player)*1.5)
         self.image = pygame.transform.rotate(self.base_player_image, (-self.angle))
-        self.rect = self.image.get_rect(center = self.hitbox_rect.center)
+        self.rect = self.image.get_rect(center=self.hitbox_rect.center)
 
     def user_input(self):
         self.velocity_x = 0
@@ -71,7 +71,7 @@ while True:
     screen.blit(background, (0, 0))
     screen.blit(player.image, player.rect)
     player.update()
-    pygame.draw.rect(screen, "red", player.hitbox_rect, width = 2)
+    pygame.draw.rect(screen, "red", player.hitbox_rect, width=2)
     pygame.draw.rect(screen, "yellow", player.rect, width=2)
 
     pygame.display.update()
